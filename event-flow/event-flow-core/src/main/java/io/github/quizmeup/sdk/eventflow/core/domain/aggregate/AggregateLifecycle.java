2c776a133e842fc27c0aa67d182bd54a43ecbccf
cnadjim <c.nadjim@gmail.com>
2025-07-07 11:15:27 +0200

    update

--- event-flow/event-flow-core/src/main/java/io/github/quizmeup/sdk/eventflow/core/domain/aggregate/AggregateLifecycle.java
+++ event-flow/event-flow-core/src/main/java/io/github/quizmeup/sdk/eventflow/core/domain/aggregate/AggregateLifecycle.java
@@ -3,7 +3,7 @@ package io.github.quizmeup.sdk.eventflow.core.domain.aggregate;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Utilitaire statique pour gérer le cycle de vie des agrégats
+ * Static utility for managing the lifecycle of aggregates.
  */
 @Slf4j
 public class AggregateLifecycle {
@@ -11,23 +11,23 @@ public class AggregateLifecycle {
     private static final ThreadLocal<Boolean> DELETED_MARKER = new ThreadLocal<>();
 
     /**
-     * Marque l'agrégat courant comme devant être supprimé.
-     * Cette méthode doit être appelée dans une méthode annotée avec @EventSourcingHandler.
+     * Marks the current aggregate as to be deleted.
+     * This method should be called within a method annotated with @EventSourcingHandler.
      */
     public static void markDeleted() {
         DELETED_MARKER.set(true);
     }
 
     /**
-     * Vérifie si l'agrégat courant a été marqué pour suppression
-     * @return true si l'agrégat est marqué pour suppression
+     * Checks if the current aggregate has been marked for deletion.
+     * @return true if the aggregate is marked for deletion
      */
     public static boolean isMarkedDeleted() {
         return Boolean.TRUE.equals(DELETED_MARKER.get());
     }
 
     /**
-     * Réinitialise l'état de marquage de suppression
+     * Resets the deletion mark state.
      */
     public static void resetDeletedMarker() {
         DELETED_MARKER.remove();
